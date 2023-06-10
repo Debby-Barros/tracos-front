7d1130d7fa989eea4c4fde73be4c5f826200e37f
tmmarquess <thiago.uchoa18@gmail.com>
2023-06-10 10:56:49 -0400

    Adding communication with backend on signUp and environment variables

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { 
+import {
   Container,
   FormContainer,
   Form,
@@ -11,64 +11,113 @@ import {
   RegisterLink,
   ButtonContainer
 } from "./styles";
+import api from "../../services/api";
 
 export function SignUp() {
+  const [name, setName] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [cpf, setCpf] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handlePasswords = (repeatedPassword) => {
+    // code to check if both passwords are the same
+    if (password === repeatedPassword) {
+      // condition
+    }
+  }
+  const handleSubmit = (event) => {
+    api.post('/api/v0/users', {
+      name: name,
+      nickname: nickname,
+      cpf: cpf,
+      phone: phone,
+      email: email,
+      password: password
+    }).catch((err) => {
+      console.error("ops! ocorreu um erro" + err);
+    })
+  }
+
+
   return (
     <Container>
       <FormContainer>
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Title>Cadastro</Title>
           <InputTitle>nome completo</InputTitle>
           <Input
             type="text"
             required
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
           />
           <InputTitle>apelido</InputTitle>
           <Input
             type="text"
             required
-            />
-            <InputTitle>CPF</InputTitle>
-            <Input
-              type="text" 
-              required
-            />
-            <InputTitle>telefone</InputTitle>
-            <Input
-              type="tel"
-              required
-            />
-            <InputTitle>email</InputTitle>
-            <Input
-              type="email"
-              required
-            />
-            <InputTitle>senha</InputTitle>
-            <Input
-              type="password"
-              required
-            />
-            <InputTitle>confirmar senha</InputTitle>
-            <Input
-              type="password"
-              required
-            />
-            <br/>
-            <ButtonContainer>
-              <RegisterLink>
-                <Link to="/" style={{
-                  textDecoration: 'none',
-                  color: '#FFB627'
-                }}>
-                  registrar
-                </Link>    
-              </RegisterLink>
-              <Button  
-                type="submit"
-              >cadastrar</Button> 
-            </ButtonContainer>
+            onChange={(event) => {
+              setNickname(event.target.value);
+            }}
+          />
+          <InputTitle>CPF</InputTitle>
+          <Input
+            type="text"
+            required
+            onChange={(event) => {
+              setCpf(event.target.value);
+            }}
+          />
+          <InputTitle>telefone</InputTitle>
+          <Input
+            type="tel"
+            required
+            onChange={(event) => {
+              setPhone(event.target.value);
+            }}
+          />
+          <InputTitle>email</InputTitle>
+          <Input
+            type="email"
+            required
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+          />
+          <InputTitle>senha</InputTitle>
+          <Input
+            type="password"
+            required
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
+          />
+          <InputTitle>confirmar senha</InputTitle>
+          <Input
+            type="password"
+            required
+            onChange={(event) => {
+              handlePasswords(event.target.value);
+            }}
+          />
+          <br />
+          <ButtonContainer>
+            <RegisterLink>
+              <Link to="/" style={{
+                textDecoration: 'none',
+                color: '#FFB627'
+              }}>
+                registrar
+              </Link>
+            </RegisterLink>
+            <Button
+              type="submit"
+            >cadastrar</Button>
+          </ButtonContainer>
 
-          </Form>
+        </Form>
       </FormContainer>
     </Container>
   )
