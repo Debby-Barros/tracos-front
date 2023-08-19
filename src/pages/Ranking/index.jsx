77fe816c49b098f648a7974195fa77c0ab99b2e1
tmmarquess <thiago.uchoa18@gmail.com>
2023-08-19 14:28:30 -0400

    adding backend communication on raking page

--- src/pages/Ranking/index.jsx
+++ src/pages/Ranking/index.jsx
@@ -1,8 +1,33 @@
 import React, { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Container, GlobalStyle } from './style'
+import { useNavigate } from "react-router-dom";
+import api from "../../services/api";
 
 export function Ranking() {
+
+    const navigate = useNavigate();
+    const [ranking, setRanking] = useState([])
+
+    useEffect(() => {
+        const jwt_token = localStorage.getItem('jwt-token');
+
+        const headers = { 'Authorization': `Bearer ${jwt_token}` }
+
+        api.get('/api/v0/users/ranking', { headers }).then((response) => {
+            const data = response.data;
+            setRanking(data)
+            // console.log(data);
+        }).catch((error) => {
+            if (error.response.status === 401) {
+                localStorage.clear();
+                navigate('/');
+            } else {
+                console.log(error);
+            }
+        })
+
+    }, [navigate])
 
     return (
         <>
